f8756beb53aef35d23220311ff9d9a3fc9157986
Naru-hub <1208ny0324@gmail.com>
2021-05-07 05:49:09 +0000

    Initial Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'd07c88e55bcbdf36c39e83a8bbeae90e6bb5bd465ebf7b62e797ca1c15319ba6dd1f65949fb40388ef7873ead40d75754124ce1cc66c9d4c36c984be2810507c'
